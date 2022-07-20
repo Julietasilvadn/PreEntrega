557bb9c99d4136949d27746bcb3973fbb88ffcec
Julieta Silva <juuli.siilva@gmail.com>
2022-07-20 01:29:25 -0300

    terminando con el administrador

--- app_entrega/admin.py
+++ app_entrega/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from app_entrega.models import Persona, Mascota, SpaAnimal
 
-# Register your models here.
+admin.site.register(Persona)
+admin.site.register(Mascota)
+admin.site.register(SpaAnimal)
