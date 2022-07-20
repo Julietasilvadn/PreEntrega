1e5b784af046d1c18c70079fea559281195ead00
Julieta Silva <juuli.siilva@gmail.com>
2022-07-20 01:05:36 -0300

    actualizacion de los templates

--- pantallas/views.py
+++ pantallas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def mostrar_inicio(request):
+    return render(request, "pantallas/inicio.html", {})
+
+def mostrar_perfil(request):
+    return render(request, "pantallas/perfil.html", {})
+
+def mostrar_reserva(request):
+    return render(request, "pantallas/reserva.html", {})
--- pre_entrega/urls.py
+++ pre_entrega/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pantallas.views import mostrar_inicio, mostrar_perfil, mostrar_reserva
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_entrega.urls'))
+    path('', include('app_entrega.urls')),
+    path('inicio/', mostrar_inicio),
+    path('perfil/', mostrar_perfil),
+    path('reserva/', mostrar_reserva)
 ]
